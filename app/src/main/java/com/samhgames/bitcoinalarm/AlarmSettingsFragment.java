85e3ff8ec723f67d7762ac1bf8aaeb0868b1670c
samhogan <sam@samhogan.net>
2018-01-06 13:22:38 -0500

    Alarm Settings is now made of preferences, started TimePreference

---
+++ app/src/main/java/com/samhgames/bitcoinalarm/AlarmSettingsFragment.java
@@ -0,0 +1,22 @@
+package com.samhgames.bitcoinalarm;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+
+/**
+ * Created by samho on 1/6/2018.
+ */
+
+public class AlarmSettingsFragment extends PreferenceFragment
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.alarm_preferences);
+    }
+
+}
